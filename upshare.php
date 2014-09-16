f379f18fd625ddf917481d0b93d2ebe17d875807
SimonTurner <SimonTurner@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-09-16 14:57:47 +0000

    Script path update
    git-svn-id: https://plugins.svn.wordpress.org/viral-sharing-buttons-by-up/trunk@991132 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- upshare.php
+++ upshare.php
@@ -286,10 +286,10 @@ function addScriptCodeToHead()
 
   if($partner_id)
   {
-	 echo '<script src="http://widget.upshare.co/v1/app/public/js/up-load.js?partnerid='.$partner_id.'" id="UPWidget"></script>';
+	 echo '<script src="https://widget.upshare.co/v1/app/public/js/up-load.js?partnerid='.$partner_id.'" id="UPWidget"></script>';
   }
   else{
-	 echo '<script src="http://widget.upshare.co/v1/app/public/js/up-load.js" id="UPWidget"></script>'; 
+	 echo '<script src="https://widget.upshare.co/v1/app/public/js/up-load.js" id="UPWidget"></script>'; 
   }
 	
 }
